38882999a421e5856ed4b9da66c934cd1af7393a
Samb092 <79867433+BusamboEnt@users.noreply.github.com>
2025-02-24 19:27:35 +0200

    Update query_samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,1 +1,10 @@
-from relationship_app.models import Author, Book, Library, Librarian
+from relationship_app.models import Library
+
+def list_books_in_library(library_name):
+    try:
+        library = Library.objects.get(name=library_name)  # Get the library by name
+        books = library.books.all()  # Retrieve all books associated with the library
+        return books
+    except Library.DoesNotExist:
+        return "Library not found."
+
