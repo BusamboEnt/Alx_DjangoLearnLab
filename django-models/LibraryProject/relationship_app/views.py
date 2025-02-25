55d7e0a964612aaa163f1194110f122ec32ac815
Samb092 <79867433+BusamboEnt@users.noreply.github.com>
2025-02-25 18:18:44 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -40,7 +40,7 @@ def register_view(request):
             return redirect("list_books")
     else:
         form = UserCreationForm()
-    return render(request, "register.html", {"form": form})
+    return render(request, "relationship_app/register.html", {"form": form})
 
 class LibraryDetailView(DetailView):
     model = Library
