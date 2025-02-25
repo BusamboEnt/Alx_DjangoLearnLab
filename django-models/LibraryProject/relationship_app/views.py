f8101a375bf2532ebf9d61b781802fb050b8d49a
Samb092 <79867433+BusamboEnt@users.noreply.github.com>
2025-02-25 07:03:43 +0200

    changes

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 from .models import Book
 from .models import Library
 
@@ -12,4 +14,12 @@ def list_books(request):
 class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
-    context_object_name = "library"
+    context_object_name = "library"    
+class BookDetailView(DetailView):
+    model = Book
+    template_name = "book_detail.html"
+    context_object_name = "book"   
+class BookListView(ListView):
+    model = Book
+    template_name = "book_list.html"
+    context_object_name = "books"       
