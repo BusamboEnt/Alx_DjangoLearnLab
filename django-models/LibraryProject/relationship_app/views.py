20dfa729f84a5e589b205524bc7c5fe7393db869
Samb092 <79867433+BusamboEnt@users.noreply.github.com>
2025-02-25 19:17:58 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth import login, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render, redirect
 from .models import UserProfile
 from .models import Book
