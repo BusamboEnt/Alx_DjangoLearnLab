22383b63e97c9fcac089678673fb741b9a85ebb8
Samb092 <79867433+BusamboEnt@users.noreply.github.com>
2025-03-02 22:56:26 +0200

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import permission_required
+from django.http import HttpResponseForbidden
+from .models import Book
 
-# Create your views here.
+@permission_required('relationship_app.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/book_list.html', {'books': books})
+
+@permission_required('relationship_app.can_create', raise_exception=True)
+def book_create(request):
+    if request.method == "POST":
+        title = request.POST['title']
+        author = request.POST['author']
+        published_date = request.POST['published_date']
+        Book.objects.create(title=title, author=author, published_date=published_date)
+        return redirect('book_list')
+    return render(request, 'relationship_app/book_form.html')
+
+@permission_required('relationship_app.can_edit', raise_exception=True)
+def book_edit(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == "POST":
+        book.title = request.POST['title']
+        book.author = request.POST['author']
+        book.published_date = request.POST['published_date']
+        book.save()
+        return redirect('book_list')
+    return render(request, 'relationship_app/book_form.html', {'book': book})
+
+@permission_required('relationship_app.can_delete', raise_exception=True)
+def book_delete(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    book.delete()
+    return redirect('book_list')
