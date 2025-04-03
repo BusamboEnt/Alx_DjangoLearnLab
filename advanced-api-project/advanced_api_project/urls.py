450547dd911617e3cffb5d439651d7f6a51f053e
Samb092 <79867433+BusamboEnt@users.noreply.github.com>
2025-04-03 19:50:15 +0200

    Update urls.py

--- advanced-api-project/advanced_api_project/urls.py
+++ advanced-api-project/advanced_api_project/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import AuthorViewSet, BookViewSet
+from .views import AuthorViewSet, BookViewSet, BookDetailsView, BookCreateView
 
 router = DefaultRouter()
 router.register(r'authors', AuthorViewSet)
 router.register(r'books', BookViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),  # Register API endpoints
+    path('', include(router.urls)), 
+    path('books/', BookListCreateView.as_view(), name='book-list-create'),
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
 ]
  
