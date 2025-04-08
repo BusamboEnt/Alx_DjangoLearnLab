1a47224b71cca8ee3b7dd46b52cb5370af3315ae
Samb092 <79867433+BusamboEnt@users.noreply.github.com>
2025-04-08 06:24:06 +0200

    corrections

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -93,21 +93,23 @@ class CommentCreateView(LoginRequiredMixin, CreateView):
     
 
 
-class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+class CommentCreateView(LoginRequiredMixin, CreateView):
     model = Comment
     form_class = CommentForm
     template_name = 'blog/comment_form.html'
 
     def form_valid(self, form):
         form.instance.author = self.request.user
+        form.instance.post = get_object_or_404(Post, pk=self.kwargs['post_id'])
         return super().form_valid(form)
 
     def get_success_url(self):
         return self.object.post.get_absolute_url()
 
-    def test_func(self):
-        comment = self.get_object()
-        return self.request.user == comment.author
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['post'] = get_object_or_404(Post, pk=self.kwargs['post_id'])
+        return context
 
 
 class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
