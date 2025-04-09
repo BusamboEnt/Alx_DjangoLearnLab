6416e95b9705b0effb3a87cf32616539963966a1
Samb092 <79867433+BusamboEnt@users.noreply.github.com>
2025-04-09 20:51:59 +0200

    Update views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,7 +1,8 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, generics
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from .models import Post, Comment
+from accounts.models import CustomUser
 from .serializers import PostSerializer, CommentSerializer
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -39,7 +40,24 @@ class FeedViewSet(viewsets.ViewSet):
 
     def list(self, request):
         user = request.user
-        followed_users = user.following.all()  # Get users that the current user follows
+        followed_users = user.following.all()  
         posts = Post.objects.filter(author__in=followed_users).order_by('-created_at')  # Get posts from followed users
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data)
+    
+class UserFeedView(generics.ListAPIView):
+    permission_classes = [IsAuthenticated]  
+
+    def get_queryset(self):
+        user = self.request.user
+        
+        following_users = user.following.all()
+        
+        posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
+
+        return posts
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = PostSerializer(queryset, many=True)
+        return Response(serializer.data)    
